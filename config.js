f077c748f03865c9cc011b8ece0d8d2f7a6dce03
yueyue <374589957@qq.com>
2019-01-04 19:57:35 +0800

    config

---
+++ config.js
@@ -0,0 +1,7 @@
+
+const DBNAME = 'sqlite3'; // sqlite3
+
+
+
+
+module.exports.dbname = DBNAME
--- db.js
+++ db.js
@@ -1,57 +1,66 @@
+const dbname = require('./config')
 
-var mysql = require("mysql")
+if(dbname === "mysql"){
+    var mysql = require("mysql")
 
-const DB = {
-    host: 'localhost',
-    user: 'root',
-    password:'zn.980514',
-    database: 'info',
-
-};
-/**
- * 提交时间
- * 姓名
- * 联系电话
- * 邮箱
- * ------
- * 开始日期
- * 截止日期
- * 原因
- * ----
- * 审批意见
- * 是否同意
- * 
- * ----
- * 删除
- * 
-        审批意见
-        是否同意
-        删除
- * 
- * SELECT `info`.`id`,
-    `info`.`name`,
-    `info`.`tel`,
-    `info`.`email`,
-    `info`.`start_date`,
-    `info`.`end_date`,
-    `info`.`reason`,
-    `info`.`advice`,
-    `info`.`is_auth`,
-    `info`.`is_del`
-FROM `info`.`info`;
-
- * 
- */
-const DBConnection = mysql.createConnection({
-    host:DB.host,
-    user:DB.user,
-    password:DB.password,
-    database:DB.database,
-    multipleStatements:true,
-
-});
-
-DBConnection.connect();
+    const DB = {
+        host: 'localhost',
+        user: 'root',
+        password:'zn.980514',
+        database: 'info',
+    
+    };
+    /**
+     * 提交时间
+     * 姓名
+     * 联系电话
+     * 邮箱
+     * ------
+     * 开始日期
+     * 截止日期
+     * 原因
+     * ----
+     * 审批意见
+     * 是否同意
+     * 
+     * ----
+     * 删除
+     * 
+            审批意见
+            是否同意
+            删除
+     * 
+     * SELECT `info`.`id`,
+        `info`.`name`,
+        `info`.`tel`,
+        `info`.`email`,
+        `info`.`start_date`,
+        `info`.`end_date`,
+        `info`.`reason`,
+        `info`.`advice`,
+        `info`.`is_auth`,
+        `info`.`is_del`
+    FROM `info`.`info`;
+    
+     * 
+     */
+    const DBConnection = mysql.createConnection({
+        host:DB.host,
+        user:DB.user,
+        password:DB.password,
+        database:DB.database,
+        multipleStatements:true,
+    
+    });
+    
+    DBConnection.connect();
+    
+}
+else{
+    if(dbname === "sqlite3"){
+        
+    }
+}
 
 module.exports.DBConnection = DBConnection;
 
